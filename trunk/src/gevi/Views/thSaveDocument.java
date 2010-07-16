d66f3e349f56ca9c02113b484a11d6e58b52228f
fredberger38 <fredberger38@18b44c90-17e5-11df-bff1-5f4ea15f2b7c>
2010-07-16 20:19:29 +0000

    Division request bug

--- trunk/src/gevi/Views/thSaveDocument.java
+++ trunk/src/gevi/Views/thSaveDocument.java
@@ -10,6 +10,8 @@ import gevi.Model.XML;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.jdom.output.Format;
 import javax.swing.JOptionPane;
 import org.jdom.output.XMLOutputter;
@@ -37,20 +39,27 @@ public class thSaveDocument extends Thread {
         {
             
         }
-        String filename = Settings.getSingleton().getProperty("LastFile");
+        String filename;
 
         if (savePeriod != 0) {
+            int index=1;
             // Tant que la mémoire libre est supérieure à 10%
             while (running) {
                 try {
                     // pause de 1s
                     Thread.sleep(savePeriod);
-
                     if (running) {
+                        filename = MainWindow.getMainWindow()._filename;
                         try {
                             if (!filename.equals("")) {
                                 XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-                                sortie.output(XML.convertDocumentToXML(Singleton.instance().getDocument()), new FileOutputStream(filename));
+                                //SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+                                index++;
+                                if (index==10)
+                                {
+                                    index=0;
+                                }
+                                sortie.output(XML.convertDocumentToXML(Singleton.instance().getDocument()), new FileOutputStream(filename+"-"+Integer.toString(index)));
                             }
                         } catch (FileNotFoundException e) {
                             JOptionPane.showMessageDialog(MainWindow.getMainWindow(), e.getLocalizedMessage());
